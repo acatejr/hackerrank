2b2a28119e0826c9f23e427c5782ec4fda88f458
Averill Cate, Jr <acate@arizona.edu>
2023-02-07 12:57:30 -0700

    More work on between two sets

--- between_two_sets.py
+++ between_two_sets.py
@@ -1,31 +1,37 @@
 """_summary_
 https://courses.lumenlearning.com/mathforliberalartscorequisite/chapter/finding-all-the-factors-of-a-number/
 """
-    
-    
+
+
 def getFactors(v):
-    
+
     results = [1, v]
-    
+
     divisor = 2
     for i in range(divisor, v+1):
         remainder = v % i
         if remainder == 0 and (i > remainder) and (i not in results):
             results.append(i)
-            
+
     results.sort()
     return results
 
 def getTotalX(a, b):
-    
-    factors = []
+
+    factors = {}
+    _factors = []
     for i in b:
-        _factors = getFactors(i)
-        factors.append(_factors)
-    print(factors)
+        for v in getFactors(i):
+            _factors.append(v)
+
+    fact_dict = {i: _factors.count(i) for i in _factors}
+    print(fact_dict)
+    # print(_factors)
 
 a = [1,2]
 b = [24,36]
 
+# a = [2,4]
+# b = [16, 32,96]
 print(getTotalX(a,b))
 
