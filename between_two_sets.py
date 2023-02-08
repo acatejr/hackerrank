e8683ef9aa16e4e40c97d0fd20704a6433358e73
Averill Cate, Jr <acate@arizona.edu>
2023-02-08 13:35:13 -0700

    Progress on between two sets

--- between_two_sets.py
+++ between_two_sets.py
@@ -5,7 +5,7 @@ https://courses.lumenlearning.com/mathforliberalartscorequisite/chapter/finding-
 
 def getFactors(v):
 
-    results = [1, v]
+    results = [v]
 
     divisor = 2
     for i in range(divisor, v+1):
@@ -18,6 +18,9 @@ def getFactors(v):
 
 def getTotalX(a, b):
 
+    if len(a) == 1 and a[0] == 1:
+        return len(getFactors(b[0]))
+    
     _factors = []
     for i in b:
         for v in getFactors(i):
@@ -25,15 +28,35 @@ def getTotalX(a, b):
     
     _factors = {i: _factors.count(i) for i in _factors}
     _factors = [i for i in _factors if _factors[i] == len(b)]
-    print(_factors)
+    # print(_factors) # [1, 2, 3, 4, 6, 12]
+    
     
     total = 0        
+    candidates = []
+    for v in a:
+        for f in _factors:
+            rem = f % v
+            if rem == 0:
+                # print(f"{f} % {v} = {rem}")
+                candidates.append(f)
+
+    candidates = {i: candidates.count(i) for i in candidates}
+    candidates = [i for i in candidates if candidates[i] == len(a)]
+    total = len(candidates)
+    
     return total
 
-a = [1, 2]
-b = [24, 36]
+# a = [2, 6]
+# b = [24, 36]
 
 # a = [2, 4]
 # b = [16, 32, 96]
+
+a = [1]
+b = [100]
+print(getTotalX(a,b))
+
+a = [1]
+b = [72, 48]
 print(getTotalX(a,b))
 
