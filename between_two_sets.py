384f28f1698f6498b17ecbda278d3ccfea08688d
Averill Cate, Jr <acate@arizona.edu>
2023-02-08 14:58:10 -0700

    Refactoring between 2 sets

--- between_two_sets.py
+++ between_two_sets.py
@@ -2,61 +2,78 @@
 https://courses.lumenlearning.com/mathforliberalartscorequisite/chapter/finding-all-the-factors-of-a-number/
 """
 
-
-def getFactors(v):
-
-    results = [v]
-
-    divisor = 2
-    for i in range(divisor, v+1):
-        remainder = v % i
-        if remainder == 0 and (i > remainder) and (i not in results):
-            results.append(i)
-
-    results.sort()
-    return results
-
 def getTotalX(a, b):
 
-    if len(a) == 1 and a[0] == 1:
-        return len(getFactors(b[0]))
+    total = 0
     
-    _factors = []
+    factors = []
     for i in b:
-        for v in getFactors(i):
-            _factors.append(v)
+        _factors = [x for x in range(1, i+1) if i % x == 0]    
+        factors += _factors
     
-    _factors = {i: _factors.count(i) for i in _factors}
-    _factors = [i for i in _factors if _factors[i] == len(b)]
-    # print(_factors) # [1, 2, 3, 4, 6, 12]
-    
+    factors = {i: factors.count(i) for i in factors}
+    factors = [i for i in factors if factors[i] == len(b)]
+    # print(factors)
     
     total = 0        
     candidates = []
-    for v in a:
-        for f in _factors:
-            rem = f % v
-            if rem == 0:
-                # print(f"{f} % {v} = {rem}")
-                candidates.append(f)
+    print(a)
+    # for v in a:
+    #     print(v)
+    #     for f in factors:
+    #         rem = f % v
+    #         if rem == 0:
+    #             # print(f"{f} % {v} = {rem}")
+    #             candidates.append(f)
 
-    candidates = {i: candidates.count(i) for i in candidates}
-    candidates = [i for i in candidates if candidates[i] == len(a)]
-    total = len(candidates)
-    
+    # candidates = {i: candidates.count(i) for i in candidates}
+    # candidates = [i for i in candidates if candidates[i] == len(a)]
+    # print(candidates)
+    # total = len(candidates)
+
     return total
 
-# a = [2, 6]
-# b = [24, 36]
+    # if len(a) == 1 and a[0] == 1:
+    #     return len(getFactors(b[0]))
+    
+    # _factors = []
+    # for i in b:
+    #     for v in getFactors(i):
+    #         _factors.append(v)
+    
+    # _factors = {i: _factors.count(i) for i in _factors}
+    # _factors = [i for i in _factors if _factors[i] == len(b)]
+    # # print(_factors) # [1, 2, 3, 4, 6, 12]
+    
+    
+    # total = 0        
+    # candidates = []
+    # for v in a:
+    #     for f in _factors:
+    #         rem = f % v
+    #         if rem == 0:
+    #             # print(f"{f} % {v} = {rem}")
+    #             candidates.append(f)
 
-# a = [2, 4]
-# b = [16, 32, 96]
+    # candidates = {i: candidates.count(i) for i in candidates}
+    # candidates = [i for i in candidates if candidates[i] == len(a)]
+    # total = len(candidates)
+    
+    # return total
+
+a = [2, 6]
+b = [24, 36]
+getTotalX(a,b)
+
+a = [2, 4]
+b = [16, 32, 96]
+getTotalX(a,b)
 
 a = [1]
 b = [100]
-print(getTotalX(a,b))
+getTotalX(a,b)
 
-a = [1]
-b = [72, 48]
-print(getTotalX(a,b))
+# a = [1]
+# b = [72, 48]
+# getTotalX(a,b)
 
