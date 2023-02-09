4fdbdbf118307b04de03801a724a8e484c33c50e
Averill Cate, Jr <acate@arizona.edu>
2023-02-09 07:20:08 -0700

    Solved between two sets

--- between_two_sets.py
+++ between_two_sets.py
@@ -4,8 +4,6 @@ https://courses.lumenlearning.com/mathforliberalartscorequisite/chapter/finding-
 
 def getTotalX(a, b):
 
-    total = 0
-    
     factors = []
     for i in b:
         _factors = [x for x in range(1, i+1) if i % x == 0]    
@@ -13,67 +11,34 @@ def getTotalX(a, b):
     
     factors = {i: factors.count(i) for i in factors}
     factors = [i for i in factors if factors[i] == len(b)]
-    # print(factors)
     
     total = 0        
     candidates = []
-    print(a)
-    # for v in a:
-    #     print(v)
-    #     for f in factors:
-    #         rem = f % v
-    #         if rem == 0:
-    #             # print(f"{f} % {v} = {rem}")
-    #             candidates.append(f)
+    for v in a:
+        for f in factors:
+            rem = f % v
+            if rem == 0:
+                candidates.append(f)
 
-    # candidates = {i: candidates.count(i) for i in candidates}
-    # candidates = [i for i in candidates if candidates[i] == len(a)]
-    # print(candidates)
-    # total = len(candidates)
+    candidates = {i: candidates.count(i) for i in candidates}
+    candidates = [i for i in candidates if candidates[i] == len(a)]    
+    total = len(candidates)
 
     return total
 
-    # if len(a) == 1 and a[0] == 1:
-    #     return len(getFactors(b[0]))
-    
-    # _factors = []
-    # for i in b:
-    #     for v in getFactors(i):
-    #         _factors.append(v)
-    
-    # _factors = {i: _factors.count(i) for i in _factors}
-    # _factors = [i for i in _factors if _factors[i] == len(b)]
-    # # print(_factors) # [1, 2, 3, 4, 6, 12]
-    
-    
-    # total = 0        
-    # candidates = []
-    # for v in a:
-    #     for f in _factors:
-    #         rem = f % v
-    #         if rem == 0:
-    #             # print(f"{f} % {v} = {rem}")
-    #             candidates.append(f)
-
-    # candidates = {i: candidates.count(i) for i in candidates}
-    # candidates = [i for i in candidates if candidates[i] == len(a)]
-    # total = len(candidates)
-    
-    # return total
-
 a = [2, 6]
 b = [24, 36]
-getTotalX(a,b)
+print(getTotalX(a,b))
 
 a = [2, 4]
 b = [16, 32, 96]
-getTotalX(a,b)
+print(getTotalX(a,b))
 
 a = [1]
 b = [100]
-getTotalX(a,b)
+print(getTotalX(a,b))
 
-# a = [1]
-# b = [72, 48]
-# getTotalX(a,b)
+a = [1]
+b = [72, 48]
+print(getTotalX(a,b))
 
