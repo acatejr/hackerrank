99a4887fd58ea0b2a8792cc617a4727950c9cda0
Averill Cate, Jr <acate@arizona.edu>
2023-02-09 07:55:22 -0700

    Started work on breaking the records

---
+++ breaking_the_records.py
@@ -0,0 +1,20 @@
+"""
+https://www.delftstack.com/howto/python/data-in-table-format-python/
+"""
+
+def breakingRecords(scores):
+    """ 
+    >>> breakingRecords([3,4,21,36,10,28,35,5,24,42])
+    [4, 0]
+    >>> breakingRecords([10,5,20,20,4,5,2,25,1])
+    [2, 4]
+    """
+    
+    results = [0,0]
+    
+    return results
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
