a96a466f2d75079abcee92640040c3e073f4e21c
Averill Cate, Jr <acatejr@gmail.com>
2023-11-22 08:14:50 -0700

    More work on python

--- design-door-mat.py
+++ design-door-mat.py
@@ -2,20 +2,35 @@ from math import floor
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 def mat(n, m):
     string = None
-    item = ".|."
-    mid_row = floor(n/2)
-    for i in range(n):
-        if i != mid_row:
-            print(f"{item}")
-        else:
-            print("WELCOME")
+    c = ".|."
+    cone = []
+
+    # Top Cone
+    for i in range(int(n/2)):
+        l = (c*i) + c + (c*i)
+        pads = "-" * (int((m - len(l))/2))
+        item = f"{pads}{l}{pads}"
+        cone.append(item)
+        print(item)
+
+    # Welcome
+    welcome = "WELCOME"
+    pads = int((m - len(welcome))/2) * "-"
+    print(f"{pads}{welcome}{pads}")
+
+    # Bottom Cone
+    cone.reverse()
+    for i in cone:
+        print(i)
+
     return string
 
 
 if __name__ == "__main__":
-    # vals = input()
+    
     # vals = "9 27"
-    vals = "11 33"
+    # vals = "11 33"
+    vals = input()
     vals = vals.split(" ")
     n = int(vals[0])
     m = int(vals[1])
