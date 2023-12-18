67e5bcfc41d86ef82147a19b1a13f73335b52115
Averill Cate, Jr <acatejr@gmail.com>
2023-12-18 06:44:47 -0700

    Finished picking-numbers

--- problem-solving/picking-numbers.py
+++ problem-solving/picking-numbers.py
@@ -1,15 +1,27 @@
 def picking_numbers(a):
     multiset = []
+    a.sort()
 
-    for i, v in enumerate(a):
-        if i+1 < len(a):
-            diff = abs(v - a[i+1])
-            if diff <= 1:
-                multiset.append(v)
-                multiset.append(a[i+1])
-                multiset += picking_numbers(a[i+1:])
+    curr_vals = []
+    for v in a:
+        if len(curr_vals) == 0:
+            curr_vals.append(v)
+        else:
+            diff = abs(v - curr_vals[0])
+            if  diff <= 1:
+                curr_vals.append(v)
+            else:
+                multiset.append(len(curr_vals))
+                curr_vals = [v]
 
-    return multiset
+    multiset.append(len(curr_vals))
+    return max(multiset)
 
 a = [4,6,5,3,3,1]
+print(picking_numbers(a))
+
+a = [1, 2, 2, 3, 1, 2]
+print(picking_numbers(a))
+
+a = [1, 1, 2, 2, 4, 4, 5, 5, 5]
 print(picking_numbers(a))
